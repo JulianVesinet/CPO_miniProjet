aaa5d872e7873b94ccbbffe4dededb3f989510c9
Julian Vesinet <julian.vesinet@epfedu.fr>
2024-11-27 10:43:44 +0100

    Création de la classe Pion représentant l'unité de base d'une combinaison, correspondant à chaque élément coloré. Chaque pion contient des informations sur sa couleur, accessibles via des accesseurs, ici 'R' pour rouge, 'B' pour bleu et 'V' pour vert. Ces couleurs sont retournées grâce à 'getCouleur()', et les lettres spécifiques à chaque couleur sont retournées grâce à 'toString()'. Les pions sont combinés pour former une proposition ou une combinaison secrète. Création du main pour Pion. Ce programme appelle les différents objets Pion, qui sont 'R', 'B' et 'V'. Puis il lance un test des méthodes utilisées dans la classe Pion, qui sont 'getCouleur()' et 'toString()'.

--- CPO_MasterMind/src/cpo_mastermind/CPO_MasterMind.java
+++ CPO_MasterMind/src/cpo_mastermind/CPO_MasterMind.java
@@ -13,8 +13,22 @@ public class CPO_MasterMind {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         // TODO code application logic here
+                // Création de quelques pions
+        Pion pionRouge = new Pion('R');
+        Pion pionBleu = new Pion('B');
+        Pion pionVert = new Pion('V');
+        
+        // Affichage des couleurs des pions
+        System.out.println("Couleur du pion rouge : " + pionRouge.toString());
+        System.out.println("Couleur du pion bleu : " + pionBleu.toString());
+        System.out.println("Couleur du pion vert : " + pionVert.toString());
+
+        // Test de la méthode getCouleur()
+        System.out.println("Couleur du pion rouge : " + pionRouge.getCouleur());
+        System.out.println("Couleur du pion bleu : " + pionBleu.getCouleur());
+        System.out.println("Couleur du pion vert : " + pionVert.getCouleur());
     }
-    
+   
 }
