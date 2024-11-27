585b6ac9190922c913a02999983a1ddaf9c48b65
Julian Vesinet <julian.vesinet@epfedu.fr>
2024-11-27 10:55:58 +0100

    Création de la classe Combinaison qui permet de gérer la représentation d'une combinaison (secrète ou proposée). Elle fournit les outils pour comparer une combinaison avec une autre et calculer les indices. Chaque combinaison sera un tableau de 4 pions (Pion[]). On retrouve dans cette classe des attibuts, comme par exemple element(Pion[]) qui est représenté sur forme de tableau qui contient 4 pions, qui représente les différentes combinaisons. On y trouve aussi l'attribut taille(int), qui permet de définir la taille de la combinaison, dans notre cas la taille est de 4. Ces combinaisons sont initialisées à partir d'un tableau de pions grâce à 'Combinaison(Pion[ ] elements)', ennsuite on retrouve 'genererAleatoire(int taille, ArrayList<Character> couleursDisponibles)' qui permet de générer nos combinaisons aléatoirement de taille 4, en sélectionnant les différentes couleurs définies pour chaques points, 'R' pour rouge, 'B' pour bleu, 'G' pour vert et 'Y' pour jaune. Chaque combinaison est comparée pion par pion, entre elles, pour déterminer le nombre de pion noir (bien placé) et blanc(mal placé) à l'aide de 'comparer(Combinaison autre)'. Pour finir, on y retrouve 'toString()', qui va nou retourner la suite des couleurs présentes dans chaque combinaison. Création du main pour Combinaison. Ce programme génère une liste de couleurs, qui sont 'R', 'B', 'G' et 'Y'. Puis il affiche la combianaison secrète qui a été créée aléatoirement par 'genererAleatoire(int taille, ArrayList<Character> couleursDisponibles)' et affiche la combinaison proposée par le joueur. Et pour finir il compare la combinaison proposée par le joueur avec celle créée aléatoirement, et affiche le nombre de blanc et de noir.

--- CPO_MasterMind/src/cpo_mastermind/CPO_MasterMind.java
+++ CPO_MasterMind/src/cpo_mastermind/CPO_MasterMind.java
@@ -4,6 +4,9 @@
  */
 package cpo_mastermind;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  *
  * @author julia
@@ -13,22 +16,34 @@ public class CPO_MasterMind {
     /**
      * @param args the command line arguments
      */
-   public static void main(String[] args) {
-        // TODO code application logic here
-                // Création de quelques pions
+    public static void main(String[] args) {
+        // Création de quelques pions
         Pion pionRouge = new Pion('R');
         Pion pionBleu = new Pion('B');
         Pion pionVert = new Pion('V');
-        
+
         // Affichage des couleurs des pions
-        System.out.println("Couleur du pion rouge : " + pionRouge.toString());
-        System.out.println("Couleur du pion bleu : " + pionBleu.toString());
-        System.out.println("Couleur du pion vert : " + pionVert.toString());
+        System.out.println("Couleur du pion rouge : " + pionRouge);
+        System.out.println("Couleur du pion bleu : " + pionBleu);
+        System.out.println("Couleur du pion vert : " + pionVert);
 
         // Test de la méthode getCouleur()
         System.out.println("Couleur du pion rouge : " + pionRouge.getCouleur());
         System.out.println("Couleur du pion bleu : " + pionBleu.getCouleur());
         System.out.println("Couleur du pion vert : " + pionVert.getCouleur());
+
+        // Création d'une combinaison statique
+        Pion[] pionsFixes = {pionRouge, pionBleu, pionVert, new Pion('Y')};
+        Combinaison combinaisonFixe = new Combinaison(pionsFixes);
+        System.out.println("Combinaison fixe : " + combinaisonFixe);
+
+        // Génération d'une combinaison aléatoire
+        ArrayList<Character> couleursDisponibles = new ArrayList<>(Arrays.asList('R', 'B', 'G', 'Y'));
+        Combinaison combinaisonAleatoire = Combinaison.genererAleatoire(4, couleursDisponibles);
+        System.out.println("Combinaison aléatoire : " + combinaisonAleatoire);
+
+        // Comparaison des deux combinaisons
+        int[] resultatComparaison = combinaisonFixe.comparer(combinaisonAleatoire);
+        System.out.println("Résultat de la comparaison : " + resultatComparaison[0] + " noirs, " + resultatComparaison[1] + " blancs");
     }
-   
 }
